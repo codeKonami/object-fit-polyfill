876b2dc3ee13f5622e6a59145d61c7c307b2476a
constancecchen <constance.chen.3@gmail.com>
2016-08-15 22:50:35 -0700

    Attempt at implementing object-position

--- objectFitPolyfill.js
+++ objectFitPolyfill.js
@@ -7,76 +7,273 @@
 * https://github.com/constancecchen/object-fit-polyfill
 *----------------------------------------*/
 
-(function($, window, undefined) {
+(function (root, factory) {
+  if (typeof define === "function" && define.amd) {
+    // AMD - registered as an anonymous module
+    define(factory);
+  } else if (typeof exports === "object") {
+    // CommonJS
+    module.exports = factory();
+  } else {
+    // Browser global
+    root.objectFitPolyfill = factory();
+  }
+})(this, function () {
+  "use strict";
 
-  $.fn.objectFitPolyfill = function(options) {
+  // If the browser does support object-fit, we don't need to continue
+  if ("objectFit" in document.documentElement.style !== false) {
+    return;
+  }
 
-    // If the browser does support object-fit, we don't need to continue
-    if ('objectFit' in document.documentElement.style !== false) {
+  // Feature test
+  var supports = !!window.getComputedStyle && !!document.querySelectorAll && !!window.addEventListener;
+
+  /**
+   * Check if the img/video element has a object-fit style
+   * @private
+   * @param $media - img/video element
+   * @param fit - string containing computed object-fit value
+   */
+  var checkObjectFit = function($media, fit) {
+    if (!fit || fit === "fill" || fit === "initial") {
+      return false;
+    }
+
+    return true;
+  };
+
+  /**
+   * Check the container's parent element to make sure it will
+   * correctly handle and clip absolutely positioned children
+   * @private
+   * @param $container - parent element
+   */
+  var checkParentContainer = function($container) {
+    var styles = window.getComputedStyle($container, null);
+    var position = styles.getPropertyValue("position");
+    var overflow = styles.getPropertyValue("overflow");
+    var display = styles.getPropertyValue("display");
+
+    if (!position || position === "static") {
+      $container.style.position = "relative";
+    }
+    if (overflow !== "hidden") {
+      $container.style.overflow = "hidden";
+    }
+    // Guesstimating that people want the parent to act like full width/height wrapper here.
+    // Mostly attempts to target <picture> elements, which default to inline.
+    if (!display || display === "inline") {
+      $container.style.display = "block";
+    }
+    if ($container.clientHeight === 0) {
+      $container.style.height = "100%";
+    }
+  };
+
+  /**
+   * Check for pre-set max-width/height or min-width/height,
+   * which can mess up image calculations
+   * @private
+   * @param $media - img/video element
+   * @param styles - computed styles of media element
+   */
+  var checkMediaConstraints = function($media, styles) {
+    var constraints = {
+      "max-width":  "none",
+      "max-height": "none",
+      "min-width":  "0px",
+      "min-height": "0px"
+    };
+
+    for (var property in constraints) {
+      var constraint = styles.getPropertyValue(property);
+
+      if (constraint !== constraints[property]) {
+        $media.style[property] = constraints[property];
+      }
+    }
+  };
+
+  /**
+   * Calculate & set object-position
+   * @private
+   * @param axis - string, either "x" or "y"
+   * @param $media - img or video element
+   * @param objectPosition - string, e.g. "50% 50%", "top bottom"
+   */
+  var setPosition = function(axis, $media, objectPosition) {
+    objectPosition = objectPosition.split(" ");
+    var position, other, start, end, side;
+
+    if (axis === "x") {
+      position = objectPosition[0];
+      other = objectPosition[1];
+      start = "left";
+      end = "right";
+      side = $media.clientWidth;
+    }
+    else if (axis === "y") {
+      position = objectPosition[1];
+      other = objectPosition[0];
+      start = "top";
+      end = "bottom";
+      side = $media.clientHeight;
+    }
+    else {
+      return; // Neither x or y axis specified
+    }
+
+    if (position === start || other === start) {
+      $media.style[start] = "0";
       return;
     }
 
-    // Setup options
-    var settings = $.extend({
-      "fit": "cover",
-      "fixContainer": false,
-    }, options);
+    if (position === end || other === end) {
+      $media.style[end] = "0";
+      return;
+    }
 
-    return this.each(function() {
+    if (position === "center" || position === "50%") {
+      $media.style[start] = "50%";
+      $media.style["margin-" + start] = (side / -2) + "px";
+      return;
+    }
 
-      var $image = $(this);
-      var $container = $image.parent();
+    // Percentage values (e.g., 30% 10%)
+    if (position.indexOf("%") >= 0) {
+      position = parseInt(position);
 
-      var coverAndPosition = function() {
-        // If necessary, make the parent container work with absolutely positioned elements
-        if (settings.fixContainer) {
-          $container.css({
-            "position": "relative",
-            "overflow": "hidden",
-          });
-        }
+      if (position < 50) {
+        $media.style[start] = position + "%";
+        $media.style["margin-" + start] = side * (position / -100) + "px";
+      }
+      else {
+        position = 100 - position;
+        $media.style[end] = position + "%";
+        $media.style["margin-" + end] = side * (position / -100) + "px";
+      }
 
-        // Mathematically figure out which side needs covering, and add CSS positioning & centering
-        $image.css({
-          "position": "absolute",
-          "height": $container.outerHeight(),
-          "width": "auto"
-        });
-
-        if (
-          settings.fit === "cover"   && $image.width() < $container.outerWidth() ||
-          settings.fit === "contain" && $image.width() > $container.outerWidth()
-        ) {
-          $image.css({
-            "top": "50%",
-            "left": 0,
-            "height": "auto",
-            "width": $container.outerWidth(),
-            "marginLeft": 0,
-          }).css("marginTop", $image.height() / -2);
-        } else {
-          $image.css({
-            "top": 0,
-            "left": "50%",
-            "marginTop": 0,
-          }).css("marginLeft", $image.width() / -2);
-        }
-      };
-
-      // Init - wait for the image to be done loading first, otherwise dimensions will return 0
-      $image.on("load", function(){
-        coverAndPosition();
-      });
-      // IE will sometimes get cache-happy and not register onload.
-      coverAndPosition();
-
-      // Recalculate widths & heights on window resize
-      $(window).resize(function() {
-        coverAndPosition();
-      });
-
-    });
+      return;
+    }
+    // Length-based values (e.g. 10px / 10em)
+    else {
+      $media.style[start] = position;
+    }
 
   };
 
-})(jQuery, window);
+  /**
+   * Calculate & set object-fit
+   * @private
+   * @param $media - img/video/picture element
+   */
+  var objectFit = function($media) {
+    // Setup
+    var styles = window.getComputedStyle($media, null);
+    var fit = styles.getPropertyValue("object-fit");
+    var position = styles.getPropertyValue("object-position");
+    var $container = $media.parentNode;
+    console.log(styles.getPropertyValue("object-fit"));
+
+    // Check if object fit has been set (typically via CSS). If not, we don't need to continue.
+    if (checkObjectFit($media, fit) === false) { return; }
+
+    // If necessary, make the parent container work with absolutely positioned elements
+    checkParentContainer($container);
+
+    // Check for max-width/height or min-width/height, which can mess up image calculations
+    checkMediaConstraints($media, styles);
+
+    // Mathematically figure out which side needs covering, and add CSS positioning & centering
+    $media.style.position = "absolute";
+    $media.style.height = "100%";
+    $media.style.width = "auto";
+
+    if (fit === "scale-down") {
+      $media.style.height = "auto";
+
+      if (
+        $media.clientWidth < $container.clientWidth &&
+        $media.clientHeight < $container.clientHeight
+      ) {
+        setPosition("x", $media, position);
+        setPosition("y", $media, position);
+      }
+      else {
+        fit = "contain";
+        $media.style.height = "100%";
+      }
+    }
+
+    if (fit === "none") {
+      $media.style.width = "auto";
+      $media.style.height = "auto";
+      setPosition("x", $media, position);
+      setPosition("y", $media, position);
+    }
+    else if (
+      fit === "cover"   && $media.clientWidth > $container.clientWidth ||
+      fit === "contain" && $media.clientWidth < $container.clientWidth
+    ) {
+      $media.style.top = "0";
+      $media.style.marginTop = "0";
+      setPosition("x", $media, position);
+    }
+    else if (fit !== "scale-down") {
+      $media.style.width = "100%";
+      $media.style.height = "auto";
+      $media.style.left = "0";
+      $media.style.marginLeft = "0";
+      setPosition("y", $media, position);
+    }
+  };
+
+  /**
+   * Initialize Plugin
+   * @public
+   */
+  var objectFitPolyfill = function() {
+    // Feature test
+    if (!supports) return;
+
+    var images = document.querySelectorAll("img");
+    var videos = document.querySelectorAll("video");
+
+    for (var i = 0; i < images.length; i ++) {
+      var $media = images[i];
+
+      if ($media.complete) {
+        objectFit($media);
+      }
+      else {
+        $media.addEventListener("load", function() {
+          objectFit(this);
+        });
+      }
+    }
+
+    for (var i = 0; i < videos.length; i ++) {
+      var $media = videos[i];
+
+      if ($media.readyState > 0) {
+        objectFit($media);
+      }
+      else {
+        $media.addEventListener("loadedmetadata", function() {
+          objectFit(this);
+        });
+      }
+    }
+  };
+
+  document.addEventListener("DOMContentLoaded", function() {
+    objectFitPolyfill();
+  });
+
+  window.addEventListener("resize", function() {
+    // TODO: This could get expensive. Add a setTimeout limiter?
+    objectFitPolyfill();
+  });
+
+});
